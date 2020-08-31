22ef338e4831c9aa3728a68dbce58121aee3832f
chicmillionaire <chicmillionaire@gmail.com>
2020-08-31 18:21:01 -0400

    Added Carousel

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./styles/App.css";
 
 import Header from "./components/Header";
+import Featured from "./components/Featured";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header  className='header'/>
+        <Featured/>
       </div>
     );
   }
---
+++ src/components/Featured.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Carousel from './Carousel';
+import "../styles/Featured.css";
+
+const Featured = () => {
+  return (
+    <div style = {{position: 'relative'}}>
+      
+      <Carousel/>
+        
+        <div className='artist_name'>
+          
+          <div className='wrapper'>            
+            Ariana Grande
+          </div>
+
+        </div>
+
+    </div>
+  );
+};
+
+export default Featured;
